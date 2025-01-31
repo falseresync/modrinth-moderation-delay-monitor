b129548cf4254811bdc364d8c9c60d5d492e3add
falseresync <falseresync@gmail.com>
2025-01-31 21:08:49 +0300

    Move base URL to an env var

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   ssr: false,
-  css: ['modern-normalize'],
-  app: {
-    baseURL: '/modrinth-moderation-delay-monitor/'
-  }
+  css: ['modern-normalize']
 })
